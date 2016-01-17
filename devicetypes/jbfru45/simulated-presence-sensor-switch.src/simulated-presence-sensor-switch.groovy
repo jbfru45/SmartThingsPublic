fa975f2f881e249efbdf71f3db898d15c728c3ed
Joe Fruland <jbfru45@gmail.com>
2016-01-17 15:55:02 -0500

    Simulated presence sensor that can also be used as a virtual switch.

---
+++ devicetypes/jbfru45/simulated-presence-sensor-switch.src/simulated-presence-sensor-switch.groovy
@@ -0,0 +1,64 @@
+/**
+ *  Copyright 2015 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Simulated Presence Sensor Switch", namespace: "jbfru45", author: "jbfru45") {
+		capability "Presence Sensor"
+        capability "Switch"
+
+		command "arrived"
+		command "departed"
+	}
+
+	tiles {
+		standardTile("presence", "device.presence", width: 2, height: 2, canChangeBackground: true) {
+			state("not present", label:'not present', icon:"st.presence.tile.not-present", backgroundColor:"#ffffff", action:"arrived")
+			state("present", label:'present', icon:"st.presence.tile.present", backgroundColor:"#53a7c0", action:"departed")
+		}
+        standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+			state "off", label: '${currentValue}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+			state "on", label: '${currentValue}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+		}
+		main "presence"
+		details(["presence", "switch"])
+	}
+}
+
+def parse(String description) {
+	def pair = description.split(":")
+	createEvent(name: pair[0].trim(), value: pair[1].trim())
+}
+
+// handle commands
+def arrived() {
+	log.trace "Executing 'arrived'"
+	sendEvent(name: "presence", value: "present")
+    sendEvent(name: "switch", value: "on")
+}
+
+
+def departed() {
+	log.trace "Executing 'departed'"
+	sendEvent(name: "presence", value: "not present")
+    sendEvent(name: "switch", value: "off")
+}
+
+def on() {
+	log.debug "Executing 'on'"
+	arrived()
+}
+
+def off() {
+	log.debug "Executing 'off'"
+	departed()
+}
